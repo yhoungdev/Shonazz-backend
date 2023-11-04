922ad3385d16c1952e0efc321a3604e47aa355a4
yhoungdev <yhoungdev@gmail.com>
2023-11-04 08:18:19 +0100

    chore: added Partnership

--- controller/partner.ts
+++ controller/partner.ts
@@ -2,6 +2,8 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const Yup = require('yup');
+import { sendMail } from "../service/nodemailer";
+
 import { Request , Response } from "express";
 const partnerValidation = Yup.object().shape({
     name: Yup.string().required(),
@@ -14,11 +16,21 @@ const partnerValidation = Yup.object().shape({
  const partners_controller = async  ( req: Request, res: Response ) => {
     try {
         const _validate_body = await partnerValidation.validate(req.body)
+        const { email:userEmail , name } = _validate_body
         const createPartners = await prisma.partner.create({
           data: {
             ..._validate_body
           },
         });
+
+        const subject: string = 'Thanks For The Partnership';
+        const body = {
+          username: name,
+        };
+        const email = userEmail
+        const template = 'email_templates/partners';
+        //@ts-ignore
+        await sendMail({ email, subject, body, template });
     
         if (!createPartners) return res.status(500).json({ message: ' Something went wrong please try again' }); 
     
